235e4e07f334494da222df2025f21952ae8c9c6a
DDangDin <Kcorelec7123@gmail.com>
2025-04-30 18:09:56 +0900

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,54 @@
+
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktor)
+    alias(libs.plugins.kotlin.plugin.serialization)
+}
+
+group = "com.peekr"
+version = "0.0.1"
+
+application {
+    mainClass = "io.ktor.server.netty.EngineMain"
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.openapi)
+    implementation(libs.ktor.server.auth)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.exposed.core)
+    implementation(libs.exposed.jdbc)
+    implementation(libs.h2)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.logback.classic)
+    implementation(libs.ktor.server.config.yaml)
+    testImplementation(libs.ktor.server.test.host)
+    testImplementation(libs.kotlin.test.junit)
+
+    // PostgreSQL JDBC Driver
+    implementation(libs.postgresql)
+
+    // Exposed ORM
+    implementation(libs.exposed.orm.core)
+    implementation(libs.exposed.orm.dao)
+    implementation(libs.exposed.orm.jdbc)
+
+    // HikariCP (Connection Pool)
+    implementation(libs.hikariCP)
+
+    // dot env
+    implementation(libs.dotenv)
+
+    // JWT
+    implementation(libs.ktor.server.auth.jwt)
+    implementation(libs.jwt)
+}
