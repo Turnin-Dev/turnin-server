a2a192b42b944c28ce618279d894a60e489ebc49
DDangDin <Kcorelec7123@gmail.com>
2025-06-02 18:09:18 +0900

    [refactor] DB 필드 속성 수정 및 DatabaseFactory.kt 수정

--- src/main/kotlin/infrastructure/persistence/Users.kt
+++ src/main/kotlin/infrastructure/persistence/Users.kt
@@ -8,7 +8,7 @@ import org.jetbrains.exposed.sql.javatime.timestamp
 object Users : LongIdTable("user") {
     val provider = enumerationByName("provider", 50, SocialLoginProvider::class)
     val providerId = varchar("provider_id", 255)
-    val name = varchar("name", 50).nullable()
+    val name = varchar("name", 50)
     val nickname = varchar("nickname", 50).nullable()
     val profileImageUrl = varchar("profile_image_url", 500).nullable()
     val introduce = text("introduce").nullable()
