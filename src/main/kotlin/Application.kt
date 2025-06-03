baf685a3e2a15dcd4fcb4c0e03f4147ca7996d18
DDangDin <Kcorelec7123@gmail.com>
2025-06-03 19:32:17 +0900

    [feat] AppConfig 추가

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,11 +1,11 @@
 package com.peekr
 
+import com.peekr.common.di.configureKoin
 import com.peekr.common.infrastructure.DatabaseFactory
 import com.peekr.common.presentation.exception.configureExceptionHandler
 import com.peekr.common.presentation.plugin.configureCallLogging
 import com.peekr.common.presentation.plugin.configureContentNegotiation
 import com.peekr.common.presentation.plugin.configureHTTP
-import com.peekr.common.presentation.plugin.configureKoin
 import com.peekr.common.presentation.plugin.configureRouting
 import com.peekr.common.presentation.plugin.configureSerialization
 import com.peekr.common.presentation.plugin.security.configureSecurity
