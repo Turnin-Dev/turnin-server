c61e57aa29bdcee928b74d3d913ed8fe33397346
DDangDin <Kcorelec7123@gmail.com>
2025-06-07 17:19:14 +0900

    [PK-24] build: 라우팅 테스트를 위한 종속성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,9 +42,15 @@ dependencies {
     implementation(libs.logback.classic)
     implementation(libs.ktor.server.config.yaml)
 
+    // Test
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
     testImplementation(libs.ktor.client.content.negotiation)
+    // Test (Mockk)
+    testImplementation(libs.mockk)
+    // Test (Client)
+    testImplementation(libs.ktor.client.core)
+    testImplementation(libs.ktor.client.cio)
 
     // Status Pages
     implementation(libs.ktor.server.host.common)
@@ -85,7 +91,4 @@ dependencies {
 
     // Flyway
     implementation(libs.flyway.core)
-
-    // Mockk
-    testImplementation(libs.mockk)
 }
