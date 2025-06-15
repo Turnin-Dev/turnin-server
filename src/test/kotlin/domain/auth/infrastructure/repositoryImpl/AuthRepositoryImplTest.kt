122685a0b8ff4778f596dbd46f7e433460b533fd
DDangDin <Kcorelec7123@gmail.com>
2025-06-15 18:28:28 +0900

    [PK-24] chore: 테스트 코드에서 runTest 블록으로 수정

--- src/test/kotlin/domain/auth/infrastructure/repositoryImpl/AuthRepositoryImplTest.kt
+++ src/test/kotlin/domain/auth/infrastructure/repositoryImpl/AuthRepositoryImplTest.kt
@@ -9,7 +9,7 @@ import kotlin.test.assertFailsWith
 import kotlin.test.assertNotNull
 import kotlin.test.assertNull
 import kotlin.test.assertTrue
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
 import org.jetbrains.exposed.exceptions.ExposedSQLException
 import org.junit.Before
 
@@ -22,7 +22,7 @@ class AuthRepositoryImplTest {
     }
 
     @Test
-    fun `save & findByProviderAndProviderId 성공 테스트`() = runBlocking {
+    fun `save & findByProviderAndProviderId 성공 테스트`() = runTest {
         TestDatabaseFactory.init()
         val savedUser = repository.save(MockAuthUser)
         assertTrue(savedUser.id > 0L)
@@ -37,7 +37,7 @@ class AuthRepositoryImplTest {
     }
 
     @Test
-    fun `findByProviderAndProviderId 실패 테스트 - 존재하지 않는 사용자`() = runBlocking {
+    fun `findByProviderAndProviderId 실패 테스트 - 존재하지 않는 사용자`() = runTest {
         val notFoundUser = repository.findByProviderAndProviderId(
             provider = SocialLoginProvider.Kakao,
             providerId = "not_found_id",
@@ -47,7 +47,7 @@ class AuthRepositoryImplTest {
     }
 
     @Test
-    fun `save 실패 테스트 - 중복된 providerId 저장 시도`() = runBlocking {
+    fun `save 실패 테스트 - 중복된 providerId 저장 시도`() = runTest {
         val savedUser = repository.save(MockAuthUser)
 
         assertTrue(savedUser.id > 0L)
