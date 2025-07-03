a4a4c6befde710c28a90082b0dc5fe513794c62e
DDangDin <Kcorelec7123@gmail.com>
2025-07-03 23:08:51 +0900

    [PK-24] refactor: created_at, updated_at 필드 필요없는 테이블은 단순 LongIdTable로 수정

--- src/main/kotlin/common/db/scheme/BlockReasons.kt
+++ src/main/kotlin/common/db/scheme/BlockReasons.kt
@@ -1,18 +1,8 @@
 package com.peekr.common.db.scheme
 
-import com.peekr.common.db.BaseEntity
-import com.peekr.common.db.BaseEntityClass
-import com.peekr.common.db.BaseLongIdTable
-import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.dao.id.LongIdTable
 
-object BlockReasons : BaseLongIdTable("block_reason") {
+object BlockReasons : LongIdTable("block_reason") {
     val code = varchar("code", 50).uniqueIndex()
     val description = text("description")
-}
-
-class BlockReasonEntity(id: EntityID<Long>) : BaseEntity(id, BlockReasons) {
-    companion object : BaseEntityClass<BlockReasonEntity>(BlockReasons)
-
-    var code by BlockReasons.code
-    var description by BlockReasons.description
 }
--- src/main/kotlin/common/db/scheme/ReportReasons.kt
+++ src/main/kotlin/common/db/scheme/ReportReasons.kt
@@ -1,18 +1,8 @@
 package com.peekr.common.db.scheme
 
-import com.peekr.common.db.BaseEntity
-import com.peekr.common.db.BaseEntityClass
-import com.peekr.common.db.BaseLongIdTable
-import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.dao.id.LongIdTable
 
-object ReportReasons : BaseLongIdTable("report_reason") {
+object ReportReasons : LongIdTable("report_reason") {
     val code = varchar("code", 50).uniqueIndex()
     val description = text("description")
-}
-
-class ReportReasonEntity(id: EntityID<Long>) : BaseEntity(id, ReportReasons) {
-    companion object : BaseEntityClass<ReportReasonEntity>(ReportReasons)
-
-    var code by ReportReasons.code
-    var description by ReportReasons.description
 }
