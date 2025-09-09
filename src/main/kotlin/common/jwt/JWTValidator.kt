9e0304ec9a69fc005b885cc943dbe3ba134974ee
DDangDin <Kcorelec7123@gmail.com>
2025-09-09 21:25:28 +0900

    [PK-44] feat: JWT 토큰에서 사용자 ID 추출하는 함수 추가 및 적용

--- src/main/kotlin/common/jwt/JWTValidator.kt
+++ src/main/kotlin/common/jwt/JWTValidator.kt
@@ -1,6 +1,10 @@
 package com.peekr.common.jwt
 
+import com.peekr.common.jwt.exception.TokenException
 import com.peekr.common.validator.PeekrValidator.validation
+import io.ktor.server.auth.jwt.JWTPrincipal
+import io.ktor.server.auth.principal
+import io.ktor.server.routing.RoutingContext
 
 object JWTValidator {
     fun validate(token: String) {
@@ -11,6 +15,21 @@ object JWTValidator {
         validation(parts.size == 2) {
             "$COMMON_TOKEN_ERROR (Ex. Bearer [토큰내용])"
         }
+    }
+
+    /**
+     * JWT 토큰 내 사용자 ID가 올바른 형식으로 포함되어있는지 확인하고 반환한다.
+     *
+     * (필요시 사용자 ID 추가 검증)
+     *
+     * @return 문자열 타입의 사용자 ID
+     *
+     * @throws TokenException.InvalidTokenException 토큰에서 사용자 ID를 찾지못하거나 올바른 형식이 아닌 경우 예외 발생
+     */
+    fun RoutingContext.getTokenUserID(): String {
+        val principal = call.principal<JWTPrincipal>() ?: throw TokenException.InvalidTokenException()
+        val authUserIdParam = principal.payload.subject ?: throw TokenException.InvalidTokenException()
+        return authUserIdParam
     }
 
     private const val COMMON_TOKEN_ERROR = "JWT 토큰 형식이 올바르지 않습니다."
--- src/main/kotlin/domain/keyword/presentation/route/KeywordRoute.kt
+++ src/main/kotlin/domain/keyword/presentation/route/KeywordRoute.kt
@@ -2,6 +2,7 @@ package com.peekr.domain.keyword.presentation.route
 
 import com.peekr.common.api.Api
 import com.peekr.common.api.Api.byPathParam
+import com.peekr.common.jwt.JWTValidator.getTokenUserID
 import com.peekr.common.jwt.exception.TokenException
 import com.peekr.common.validator.ValidatorException
 import com.peekr.domain.core.model.UserId
@@ -9,8 +10,6 @@ import com.peekr.domain.keyword.application.usecase.UserKeywordUseCase
 import com.peekr.domain.keyword.presentation.dto.toResponse
 import io.github.smiley4.ktoropenapi.get
 import io.github.smiley4.ktoropenapi.route
-import io.ktor.server.auth.jwt.JWTPrincipal
-import io.ktor.server.auth.principal
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 
@@ -20,10 +19,8 @@ fun Route.keywordRoutes(route: Api.V1.Keyword, userKeywordUseCase: UserKeywordUs
         description = "Keyword API"
     }) {
         get(route.ROUTE.byPathParam("userId"), { }) {
-            val principal = call.principal<JWTPrincipal>() ?: throw TokenException.InvalidTokenException()
-            val authUserIdParam = principal.payload.subject ?: throw TokenException.InvalidTokenException()
+            val authUserIdParam = getTokenUserID()
             val authUserId = UserId.from(authUserIdParam)
-
             val userIdParam = call.pathParameters["userId"]
             val userId = try {
                 UserId.from(userIdParam)
