3bb0b7f68190f177afabd137099c6204277ef77a
DDangDin <Kcorelec7123@gmail.com>
2025-09-13 22:20:29 +0900

    test: PeekrValidator 테스트 코드 수정 & 추가

--- src/test/kotlin/common/validator/PeekrValidatorTest.kt
+++ src/test/kotlin/common/validator/PeekrValidatorTest.kt
@@ -8,6 +8,7 @@ import kotlin.test.assertFailsWith
 import kotlin.test.assertTrue
 import org.junit.Test
 import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertNotNull
 
 class PeekrValidatorTest {
     @Test
@@ -28,7 +29,20 @@ class PeekrValidatorTest {
     }
 
     @Test
-    fun `validation 예외 테스트`() {
+    fun `validation 예외 테스트 - IllegalArgumentException 예외 발생 시 ValidatorException 예외로 변환한다`() {
+        val errorMessage = "exception occurred"
+        val exception = assertFailsWith<ValidatorException> {
+            validation(false) {
+                throw IllegalArgumentException(errorMessage)
+            }
+        }
+
+        assertNotNull(exception.message)
+        assertTrue(exception.message!!.contains(errorMessage))
+    }
+
+    @Test
+    fun `validation 예외 테스트 - IllegalArgumentException가 아닌 예외 발생 시 ValidationDefault 예외 발생`() {
         val errorMessage = "exception occurred"
         val exception = assertFailsWith<ApiException> {
             validation(false) {
@@ -36,7 +50,7 @@ class PeekrValidatorTest {
             }
         }
 
-        assertEquals(CommonErrorCode.Unexpected, exception.errorCode)
+        assertEquals(CommonErrorCode.ValidationDefault.code, exception.errorCode.code)
         assertTrue(exception.message.contains(errorMessage))
     }
 }
