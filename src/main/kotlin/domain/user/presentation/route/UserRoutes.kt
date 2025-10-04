c05263ecb9a0e1e3f7efdcb39b6d24fb3d4894d7
DDangDin <Kcorelec7123@gmail.com>
2025-10-05 00:51:06 +0900

    [PK-49] docs: 사용자 프로필 조회 API 명세서 수정

--- src/main/kotlin/domain/user/presentation/route/UserRoutes.kt
+++ src/main/kotlin/domain/user/presentation/route/UserRoutes.kt
@@ -94,8 +94,8 @@ fun RouteConfig.getUserByIdDocs() {
 
 fun RouteConfig.getUserProfileByIdDocs() {
     summary = "사용자 프로필 조회"
-    description = "사용자 ID로 사용자 프로필을 조회한다.\n" +
-        "사용자 조회와 다른점은 사용자 데이터에 추가 데이터가 포함된다."
+    description = "사용자 ID로 사용자 프로필을 조회한다." +
+        "(사용자 조회와 다른점은 사용자 데이터에 추가 데이터가 포함된다)"
     response {
         code(HttpStatusCode.OK) {
             body<UserResponse> {
