54ac91b98b1819267d644ab32ad0a52645b3a6c1
DDangDin <Kcorelec7123@gmail.com>
2025-11-15 01:08:01 +0900

    [PK-49] chore: Unused import remove

--- src/test/kotlin/domain/user/application/usecase/GetUserProfileUseCaseTest.kt
+++ src/test/kotlin/domain/user/application/usecase/GetUserProfileUseCaseTest.kt
@@ -1,9 +1,7 @@
 package com.peekr.domain.user.application.usecase
 
 import com.peekr.common.model.DisplayId
-import com.peekr.common.model.DisplayId.Companion.invoke
 import com.peekr.common.model.Name
-import com.peekr.common.model.Name.Companion.invoke
 import com.peekr.common.model.UserId
 import com.peekr.domain.user.application.dto.toDto
 import com.peekr.domain.user.domain.model.RoleForUser
