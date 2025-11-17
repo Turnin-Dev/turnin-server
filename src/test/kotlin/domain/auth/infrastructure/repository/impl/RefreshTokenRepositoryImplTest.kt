c8c399e62bd7ee938ae73076937ee78f30165e38
DDangDin <Kcorelec7123@gmail.com>
2025-11-17 22:43:24 +0900

    refactor: 코드 리뷰 반영
    - LoginUseCase 개선
    - 테스트 코드 수정

--- src/test/kotlin/domain/auth/infrastructure/repository/impl/RefreshTokenRepositoryImplTest.kt
+++ src/test/kotlin/domain/auth/infrastructure/repository/impl/RefreshTokenRepositoryImplTest.kt
@@ -15,13 +15,13 @@ import java.time.Instant
 import kotlin.test.AfterTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 import kotlin.test.assertNotNull
 import kotlin.test.assertNull
 import kotlin.test.assertTrue
 import kotlinx.coroutines.test.runTest
 import org.jetbrains.exposed.sql.upsert
 import org.junit.Before
-import org.junit.jupiter.api.assertThrows
 
 class RefreshTokenRepositoryImplTest {
     private val refreshTokenRepository = RefreshTokenRepositoryImpl()
@@ -103,7 +103,7 @@ class RefreshTokenRepositoryImplTest {
 
     @Test
     fun `save 실패 테스트 - 사용자가 존재하지 않는 경우`() = runTest {
-        assertThrows<DatabaseException.ForeignKeyViolationException> {
+        assertFailsWith<DatabaseException.ForeignKeyViolationException> {
             refreshTokenRepository.save(UserId(1L), TEST_REFRESH_TOKEN)
         }
     }
