05c32f2d268046baa3f55e3203766c2f42116f24
DDangDin <Kcorelec7123@gmail.com>
2025-11-24 17:33:16 +0900

    [PK-60] test: GetUserProfileUseCaseTest 테스트 코드 추가

--- src/test/kotlin/domain/user/application/usecase/GetUserProfileUseCaseTest.kt
+++ src/test/kotlin/domain/user/application/usecase/GetUserProfileUseCaseTest.kt
@@ -2,13 +2,10 @@ package com.peekr.domain.user.application.usecase
 
 import com.peekr.common.model.FriendshipStatus
 import com.peekr.common.model.Introduce
-import com.peekr.common.model.Introduce.Companion.invoke
 import com.peekr.common.model.Name
-import com.peekr.common.model.Name.Companion.invoke
 import com.peekr.common.model.Role
 import com.peekr.common.model.SocialLoginProvider
 import com.peekr.common.model.id.DisplayId
-import com.peekr.common.model.id.DisplayId.Companion.invoke
 import com.peekr.common.model.id.UserId
 import com.peekr.domain.user.domain.model.User
 import com.peekr.domain.user.domain.provider.FriendProvider
