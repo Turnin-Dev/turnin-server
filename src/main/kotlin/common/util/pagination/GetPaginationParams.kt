a6b0ec115cf4d0e2b41e7b0b5cf435f87c1692b7
DDangDin <Kcorelec7123@gmail.com>
2025-12-09 18:55:06 +0900

    [PK-63] feat: 페이지네이션(오프셋 기반) 유틸 수정

--- src/main/kotlin/common/util/pagination/GetPaginationParams.kt
+++ src/main/kotlin/common/util/pagination/GetPaginationParams.kt
@@ -1,23 +1,26 @@
 package com.peekr.common.util.pagination
 
 import com.peekr.common.validator.inputValidationAndReturn
-import io.ktor.server.application.ApplicationCall
+import io.ktor.server.routing.RoutingContext
 
 /**
  * 페이지네이션(오프셋 기반) 파라미터를 가져온다.
  */
-fun ApplicationCall.getPaginationParams(): PaginationParams {
-    val page = parameters["page"]
+fun RoutingContext.getPaginationParams(): PaginationParams {
+    val page = call.request.queryParameters["page"]
         ?.toLongOrNull()
         .inputValidationAndReturn("PaginationParams(page)")
-    val pageSize = parameters["pageSize"]
-        ?.toLongOrNull()
+    val size = call.request.queryParameters["size"]
+        ?.toIntOrNull()
         .inputValidationAndReturn("PaginationParams(pageSize)")
 
-    if (page > 1) throw IllegalArgumentException("Page number must be positive.")
-    if (pageSize > 1) throw IllegalArgumentException("PageSize number must be positive.")
+    if (page < 1) throw IllegalArgumentException("Page number must be positive.")
+    if (size < 1) throw IllegalArgumentException("Page size number must be positive.")
 
-    val offset = (page - 1) * pageSize
+    val offset = (page - 1) * size
 
-    return PaginationParams(page, pageSize, offset)
+    return PaginationParams(
+        offset = offset,
+        size = size,
+    )
 }
--- src/main/kotlin/common/util/pagination/PaginationParams.kt
+++ src/main/kotlin/common/util/pagination/PaginationParams.kt
@@ -3,12 +3,10 @@ package com.peekr.common.util.pagination
 /**
  * 페이지네이션(오프셋 기반) 파라미터
  *
- * @property page 페이지 번호
- * @property pageSize 페이지 크기
  * @property offset 오프셋
+ * @property size 페이지 크기
  */
 data class PaginationParams(
-    val page: Long,
-    val pageSize: Long,
     val offset: Long,
+    val size: Int,
 )
