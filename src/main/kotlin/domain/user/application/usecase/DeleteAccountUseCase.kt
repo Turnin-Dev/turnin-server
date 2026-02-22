20dfad287e7664aaabc79166a2bba2ba9a379970
DDangDin <Kcorelec7123@gmail.com>
2026-02-22 18:57:03 +0900

    [PK-109] refactor: Report 정책 수정

--- src/main/kotlin/domain/user/application/usecase/DeleteAccountUseCase.kt
+++ src/main/kotlin/domain/user/application/usecase/DeleteAccountUseCase.kt
@@ -1,5 +1,6 @@
 package com.peekr.domain.user.application.usecase
 
+import com.peekr.common.db.suspendTransaction
 import com.peekr.common.model.id.UserId
 
 /**
@@ -15,12 +16,12 @@ class DeleteAccountUseCase {
      *
      * @param userId 사용자 ID
      */
-    suspend operator fun invoke(userId: Long) {
+    suspend operator fun invoke(userId: Long) = suspendTransaction {
         val userIDVO = UserId(userId)
 
         // 1. 개인 정보 삭제/비식별화 (사용자의 모든 데이터 삭제/비식별화)
 
-        // 2. 로그아웃 과정 수행 (리프레쉬, FCM 토큰 모두 삭제)
+        // 2. 로그아웃 과정 수행 (리프레쉬, FCM 토큰 모두 삭제 포함)
 
         // 2. 파일 서버 정리 (스토리지 서버에 있는 사용자의 데이터를 모두 삭제)
     }
