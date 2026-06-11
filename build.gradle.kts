120b0872d57eebee28ed4e0c17c88412231fa7f5
DDangDin <Kcorelec7123@gmail.com>
2026-06-11 18:10:16 +0900

    test: 계정 삭제 배치 통합 테스트 코드 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -173,6 +173,7 @@ dependencies {
     implementation(libs.koin.logger)
     implementation(libs.koin.annotations)
     ksp(libs.koin.ksp.compiler)
+    testImplementation(libs.koin.test)
 
     // Flyway
     implementation(libs.flyway.core)
